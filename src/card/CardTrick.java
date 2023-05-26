7478f5007085f355f98629880217ef8c7a1c41ef
reeture <133915493+reeture@users.noreply.github.com>
2023-05-26 19:27:07 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -15,6 +15,11 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 class Card {
     private String suit;
     private String value;
@@ -50,3 +55,60 @@ class Card {
         return value + " of " + suit;
     }
 }
+
+class MagicHand {
+    private List<Card> cards;
+
+    public MagicHand() {
+        cards = new ArrayList<>();
+    }
+
+    public void fillHand() {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] values = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+
+        List<Card> deck = new ArrayList<>();
+        for (String suit : suits) {
+            for (String value : values) {
+                deck.add(new Card(suit, value));
+            }
+        }
+
+        Random random = new Random();
+        for (int i = 0; i < 7; i++) {
+            int index = random.nextInt(deck.size());
+            cards.add(deck.remove(index));
+        }
+    }
+
+    public Card pickCard() {
+        Card luckyCard = new Card();
+        luckyCard.setValue("7");  // Set the card number as desired
+        luckyCard.setSuit("Spades");  // Set the card suit as desired
+        return luckyCard;
+    }
+
+    public boolean searchCard(Card userCard) {
+        for (Card card : cards) {
+            if (card.getSuit().equalsIgnoreCase(userCard.getSuit()) && card.getValue().equalsIgnoreCase(userCard.getValue())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public class CardTrick {
+    public static void main(String[] args) {
+        MagicHand magicHand = new MagicHand();
+        magicHand.fillHand();
+
+        System.out.println("Magic hand filled with 7 random cards.");
+
+        Card userCard = magicHand.pickCard();
+
+        boolean foundMatch = magicHand.searchCard(userCard);
+        if (foundMatch) {
+            System.out.println("Congratulations! You found a match!");
+        } else {
+            System.out.println("Sorry, no match found.");
